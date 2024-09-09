3e4259c5cd379b86195f6c5c2d311309c6cc5a24
Muhammad-javed440 <meherjaved440@gmail.com>
2024-09-09 05:19:35 +0500

    Initial commit from Create Next App

---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+;
+import DigitalClock from "@/components/ui/DigitalClock";
+import React from "react";
+export default function Home() {
+  return ( 
+    <div>
+<DigitalClock/>
+    </div>
+  );
+}
